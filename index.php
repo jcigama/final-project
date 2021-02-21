bfc7a462f764a00a416f00b25455b1064b58f593
jcigama <jcigama77@yahoo.com>
2021-02-20 22:55:12 -0500

    added default route

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Start a session
+session_start();
+
+//Require files
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run Fat-Free
+$f3->run();
