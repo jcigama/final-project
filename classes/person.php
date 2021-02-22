7cbc9d6d5e0e8a427a72a69014b32cc89efbdfa8
jcigama <jcigama77@yahoo.com>
2021-02-22 01:16:44 -0500

    pushing person.php

---
+++ classes/person.php
@@ -0,0 +1,16 @@
+<?php
+
+class Person {
+
+    private $_food;
+    private $_meal;
+    private $_condiments;
+
+    /**
+     * Person constructor.
+     */
+    public function __construct()
+    {
+    }
+
+}
